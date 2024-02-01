a9cb9fd7e98fe6dca38f59ebe5c9637234391e53
Jayasanka <33048395+jayasanka-sack@users.noreply.github.com>
2024-02-01 13:01:47 +0530

    Add refactors

--- packages/apps/esm-login-app/src/login.resource.ts
+++ packages/apps/esm-login-app/src/login.resource.ts
@@ -96,6 +96,21 @@ export function useLoginLocations(
   return memoizedLocations;
 }
 
+export async function performLogin(username: string, password: string): Promise<{ data: Session }> {
+  const abortController = new AbortController();
+  const token = window.btoa(`${username}:${password}`);
+  const url = `/ws/rest/v1/session`;
+
+  return openmrsFetch(url, {
+    headers: {
+      Authorization: `Basic ${token}`,
+    },
+    signal: abortController.signal,
+  }).then((res) => {
+    refetchCurrentUser();
+    return res;
+  });
+
 export function useValidateLocationUuid(userPreferredLocationUuid: string) {
   const url = userPreferredLocationUuid ? `${fhirBaseUrl}/Location?_id=${userPreferredLocationUuid}` : null;
   const { data, error, isLoading } = useSwrImmutable<FetchResponse<LocationResponse>>(url, openmrsFetch, {
